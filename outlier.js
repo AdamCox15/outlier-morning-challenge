636cc3699da13f4de460646c8f54ad54ccfd9b8a
Adam Cox <adamcox.dev@gmail.com>
2022-09-12 09:52:51 -0600

    Final commit before review

--- outlier.js
+++ outlier.js
@@ -1,1 +1,19 @@
 //Find the outlier
+const outliersArray = [160, 3, 1719, 19, 11, 13, -21]
+const outlier = (arr) => {
+    let arrEven = []
+    let arrOdd = []
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] % 2 === 0){
+        arrEven.push(arr[i])
+    } else{
+      arrOdd.push(arr[i]);
+    }
+}
+if(arrEven.length > arrOdd.length){
+    return arrOdd;
+} else {
+    return arrEven
+}
+}
+console.log(outlier(outliersArray));
